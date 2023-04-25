3938b7e78d145ec4cae3f641f2fa3cd920f4bcb2
sunshineplan <sunshineplan@users.noreply.github.com>
2023-04-25 17:08:17 +0800

    cmd: Update

--- cmd/main.go
+++ cmd/main.go
@@ -6,13 +6,24 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/sunshineplan/shortcut"
 )
 
-var m shortcut.Map
+var (
+	m shortcut.Map
 
-var list = flag.Bool("list", false, "list shortcuts")
+	menu                 []shortcut.Key
+	maxKeyLength, number int
+)
+
+var (
+	list = flag.Bool("list", false, "list shortcuts")
+	id   = flag.Int("run", 0, "run shortcut")
+)
 
 func init() {
 	var path string
@@ -36,21 +47,50 @@ func init() {
 	if err := m.FromFile(path); err != nil {
 		log.Fatal(err)
 	}
+	m.Range(func(k shortcut.Key, s shortcut.Shortcut) bool {
+		if l := len(k); l > maxKeyLength {
+			maxKeyLength = l
+		}
+		menu = append(menu, k)
+		return true
+	})
+	for n := len(menu); n != 0; number++ {
+		n /= 10
+	}
+	sort.Slice(menu, func(i, j int) bool { return menu[i] < menu[j] })
 }
 
 func main() {
+	if len(menu) == 0 {
+		log.Print("no shortcut loaded")
+		return
+	}
+
 	flag.Parse()
 	if *list {
-		m.Range(func(k shortcut.Key, s shortcut.Shortcut) bool {
-			fmt.Printf("%s:\n\t%s\n", k, s)
-			return true
-		})
+		print()
+		return
+	}
+	if *id > 0 {
+		if err := run(*id); err != nil {
+			log.Fatal(err)
+		}
 		return
 	}
 
 	switch flag.NArg() {
 	case 0:
-		flag.PrintDefaults()
+		print()
+		fmt.Print("\nPlease choose: ")
+		var choice string
+		fmt.Scan(&choice)
+		n, err := strconv.Atoi(choice)
+		if err != nil {
+			log.Fatalln("Bad choice:", choice)
+		}
+		if err := run(n); err != nil {
+			log.Fatal(err)
+		}
 	default:
 		if cmd, ok := m.Load(shortcut.Key(flag.Arg(0))); ok {
 			var a []any
@@ -63,5 +103,34 @@ func main() {
 		} else {
 			log.Fatalf("Shortcut %s not found", flag.Arg(0))
 		}
+	}
+}
+
+func print() {
+	for i, key := range menu {
+		sc, _ := m.Load(key)
+		fmt.Printf(fmt.Sprintf("%%%dd", number)+". %s  %s  %s\n", i+1, key, strings.Repeat(" ", maxKeyLength-len(key)), sc)
+	}
+}
+
+func run(choice int) error {
+	if choice < 1 || choice > len(menu) {
+		return fmt.Errorf("bad choice: %d", choice)
+	}
+	key := menu[choice-1]
+	sc, _ := m.Load(key)
+	fmt.Println("Run", key)
+	if args := sc.Args(); args == 0 {
+		return sc.Run()
+	} else {
+		fmt.Println(sc)
+		var a []any
+		for n := 1; n <= args; n++ {
+			fmt.Printf("Please input argument %d: ", n)
+			var arg string
+			fmt.Scan(&arg)
+			a = append(a, arg)
+		}
+		return sc.Run(a...)
 	}
 }
