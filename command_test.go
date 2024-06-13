4f8a19f38064ae507981c911370f23e7849fbd77
sunshineplan <sunshineplan@users.noreply.github.com>
2024-06-13 16:02:04 +0800

    Support loop argument

--- command_test.go
+++ command_test.go
@@ -36,3 +36,23 @@ func TestCommand(t *testing.T) {
 		t.Errorf("expected %q; got %q", expect, res)
 	}
 }
+
+func TestLoopCommand(t *testing.T) {
+	self, err := os.Executable()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	a := Command(self, -1, "-%s")
+	cmd, _ := a.cmd(context.Background(), "a", "b", "c")
+	if cmd, expect := cmd.String(), self+" -a -b -c"; cmd != expect {
+		t.Errorf("expected %q; got %q", expect, cmd)
+	}
+	b := Commands(Command(self, -1, "%s"), Command(self, 1, "%s"))
+	if args := b.Args(); args != -1 {
+		t.Errorf("expected -1; got %d", args)
+	}
+	if err := b.Run(); err == nil || err.Error() != "commands do not support loop arguments" {
+		t.Errorf("expected error; got %v", err)
+	}
+}
